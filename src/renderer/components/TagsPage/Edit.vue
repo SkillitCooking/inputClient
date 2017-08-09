c5ef42fa23abb5f108ec9f5586d7ad291051521b
Dane Louis Bratz <danebratz@gmail.com>
2017-08-08 18:03:47 -0700

    action nav fixed

---
+++ src/renderer/components/TagsPage/Edit.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Edit Tags</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scopeds>
+
+</style>
--- src/renderer/components/lib/ActionNav.vue
+++ src/renderer/components/lib/ActionNav.vue
@@ -1,15 +1,25 @@
 <template>
-  <nav class="navbar">
-      <div class="navbar-brand">
-        <router-link to="/units" class="navbar-item">blah blah {{ path }} </router-link>
-        <a class="navbar-item">Hellow</a>
-      </div>
+  <nav class="breadcrumb has-dot-separator">
+    <ul>
+      <li v-bind:class="{'is-active': isCreate}" v-on:click="toggleCreate"><a>Create</a></li>
+      <li v-bind:class="{'is-active': isEdit}" v-on:click="toggleEdit"><a>Edit</a></li>
+    </ul>
   </nav>
 </template>
 
 <script>
 export default {
-  props: ['path']
+  props: ['isCreate', 'isEdit'],
+  methods: {
+    toggleCreate() {
+      this.$emit('update:isCreate', true);
+      this.$emit('update:isEdit', false);
+    },
+    toggleEdit() {
+      this.$emit('update:isCreate', false);
+      this.$emit('update:isEdit', true);
+    }
+  }
 }
 </script>
 
